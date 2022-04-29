20e07773250494617e51d437aa73783c711c4695
ryanweiler92 <ryanweiler92@gmail.com>
2022-04-29 19:33:55 -0400

    display data on homepage

--- client/src/components/Display20.js
+++ client/src/components/Display20.js
@@ -7,18 +7,31 @@ export default function Dispaly20(props){
     <Row>
         {props.gameData?.map((game) => {
             return (
-        <Col key={game.id}>
-            <Card  className="single-deal-card">
-                <img src={game.image} className="card-img-top" />
+        <Col key={game.id} className="col-md-4 col-lg-3 col-xl-3 mt-4">
+            <Card  className="single-deal-card h-100">
+                <img src={game.image} className="card-img-top display20-img" />
                 <Card.Body>
-                    <Card.Title>{game.name}</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">Release Date: {game.released}</Card.Subtitle>
+                    <Row>
+                        <Col className="col-lg-12 mx-auto">
+                            <h5 className="text-center">{game.name}</h5>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className="col-lg-12 mx-auto">
+                            <h6 className="text-center text-muted">Release Date: {game.released}</h6>
+                        </Col>
+                    </Row>
                     <ul className="list-group list-group-flush">
-                        <li className="list-group-item">Platforms: {game.parentPlatforms?.map((platform) => {
-                        return (<> {platform.platform.name} </>)})} </li>
-                        <li className="list-group-item">Steam Rating: % Steam Rating Count: </li>
-                        <li className="list-group-item">Metacritic Rating: </li>
+                        <li className="list-group-item text-center">{game.parentPlatforms?.map((platform) => {
+                        if(platform.platform.name === 'PlayStation') return (<i className="fa-brands fa-playstation fa-lg"></i>)
+                        else if (platform.platform.name === 'Xbox') return (<i className="fa-brands fa-xbox fa-lg"></i>)
+                        else if (platform.platform.name === 'Nintendo') return (<i className="fab fa-nintendo-switch fa-lg"></i>)  
+                        else return (<i className="fa-solid fa-desktop fa-lg"></i>)})} </li>
+                        <li className="list-group-item">Metacritic Rating: {game.metacritic} </li>
                     </ul>
+                    <Row className="d-flex align-items-center justify-content-center">
+                    <button className="view-game-btn"><i className="fa-solid fa-gamepad"></i> View Game</button>
+                    </Row>
                 </Card.Body>
             </Card>
         </Col>
